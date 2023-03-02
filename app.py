4905dc8b32805f2c6ec0a62aecef16b662979d5e
DenilDenilson <jordanpreseleccion@gmail.com>
2023-03-02 12:35:15 -0500

    Trabajo con métodos HTTP

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -42,3 +42,33 @@ def dashboard(name=None):
 @app.route("/ejemplo")
 def example():
     return redirect(url_for("about"))
+
+# Trabajando con métodos HTTP
+# El método GET es el que se usa por defecto
+
+# P O S T
+
+@app.route("/loginpost", methods=["GET", "POST"]) # Se colocan los métodos permitidos
+def loginPost():
+    print(request)
+    if request.method == "POST":
+        username = request.form["username"] # name del input
+        password = request.form["password"]
+
+        if username and password:
+            return redirect(url_for("loginEnter", name=username, password=password))
+    
+    
+    return """
+        <form method="POST">
+            <h3>Ingresa tus datos</h3>
+            <input type="text" name="username" /> <br/> <br/>
+            <input type="password" name="password" /> <br/> <br/>
+            <button type="submit" >Enviar</button>
+        </form>
+    """
+# @app.route("/loginenter/<name>")
+@app.route("/loginenter")
+def loginEnter():
+    print(request.args.get('password'))
+    return f'<h1>Bienvenido {request.args.get("name")} desde el form tu contraseña es "{request.args.get("password")}" xd</h1>'
